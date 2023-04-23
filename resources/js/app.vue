e6166ccf55115876bfa4f12d04fd34b4612a474e
unknown <alexorlow17@gmail.com>
2023-04-23 00:24:42 -0400

    add a login page component

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,46 +1,78 @@
 <template>
-    <div>
+    <div class="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-white">
+          Sign in to your account
+        </h2>
     
-      <form onSubmit="preventDefault">  <input v-model="newTask"  placeholder="Add a task"><button @click="addTask">add</button></form>
-
-      <ul>
-        <li v-for="(task, index) in tasks" :key="index" style="text-decoration: {{task.completed ? none:line-through}};">
-          <input type="checkbox" v-model="task.completed" @change="completeTask(task)">
-          {{ task.text }}
-          <button @click="removeTask(index)">Remove</button>
-        </li>
-      </ul>
+      </div>
+  
+      <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div class="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form class="space-y-6" @submit.prevent="submitForm">
+            <div>
+              <label for="email" class="block text-sm font-medium text-white">
+                Email address
+              </label>
+              <div class="mt-1">
+                <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-700 text-white">
+              </div>
+            </div>
+  
+            <div>
+              <label for="password" class="block text-sm font-medium text-white">
+                Password
+              </label>
+              <div class="mt-1">
+                <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-700 text-white">
+              </div>
+            </div>
+  
+            <div class="flex items-center justify-between">
+              <div class="flex items-center">
+                <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+                <label for="remember_me" class="ml-2 block text-sm text-white">
+                  Remember me
+                </label>
+              </div>
+  
+              <div class="text-sm">
+                <a href="#" class="font-medium text-indigo-400 hover:text-indigo-500">
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+  
+            <div>
+              <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </template>
   
   <script>
-import { looseIndexOf } from '@vue/shared'
-
   export default {
-    data() {
-      return {
-        newTask: '',
-        tasks: [],
-      }
-    },
     methods: {
-      addTask() {
-        if (this.newTask.trim() === '') return
-        this.tasks.push({
-          text: this.newTask.trim(),
-          completed: false,
-        })
-        this.newTask = ''
-      },
-      removeTask(index) {
-        this.tasks.splice(index, 1)
-      },
-      completeTask(task) {
-        
-        this.tasks[this.tasks.indexOf()].completed = true;
+      submitForm() {
+        // Handle form submission here
       },
     },
-  }
+  };
   </script>
+  
+  <style>
+  
+  
+  body {
+    background: linear-gradient(180deg, #5f2368 0%, #1c1a29 100%);
  
+  }
+  
+ 
+  </style>
+  
   
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
- ....
+<script src="https://cdn.tailwindcss.com"></script>
         @vite('resources/css/app.css')
 </head>
 <body>
